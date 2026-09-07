9628d315c3bd6d4d544e0d61f6455f04815c63a8
Talha <talhay.sa@gmail.com>
2026-09-07 12:58:09 +0300

    Add spacing between 'סוגי התראה' expander and 'ערוצי התראה' section in sidebar

--- dashboard.py
+++ dashboard.py
@@ -1760,7 +1760,8 @@ with st.sidebar:
             )
 
     st.markdown(
-        '<b><span style="display:inline-block; transform:scaleX(-1);">📢</span> ערוצי התראה</b>',
+        '<b style="display:inline-block; margin-top:14px;">'
+        '<span style="display:inline-block; transform:scaleX(-1);">📢</span> ערוצי התראה</b>',
         unsafe_allow_html=True,
     )
     ch1, ch2, _ch3 = st.columns([1.3, 1.3, 1.4])
